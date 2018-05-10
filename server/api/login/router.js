8f74b6e469babe14257d816b296219293f2d22b0
jesusvilla <jrvr1993@gmail.com>
2018-05-09 23:57:56 -0500

    Server: agregando router a la estructura

---
+++ server/api/login/router.js
@@ -0,0 +1,9 @@
+module.exports = {
+    'Ingresar': {// Nombre del controlador: [controlador.]metodo que se va a ejecutar
+        method: 'POST',
+        // url: 'Ingresar' // Url de la petición. Si no se coloca es el mismo que el nombre de la propiedad
+        // schema: {}
+        // beforeHandler(request, response, next)
+        // handler(request, response, next)
+    }
+}
